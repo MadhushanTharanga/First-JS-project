4d95efc3f481f977d3274d30f325bc4a330e67d1
MadhushanTharanga <madhuwa07@gmail.com>
2023-10-29 20:21:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// function printData(name ="madhushan"){
+
+//     console.log( 'heloo '+name );
+    
+// }
+
+// printData('hutooo');
+// printData('pakaaa');  
+// printData();  
+
+const sum = function(num1,num2){
+    console.log(num1+num2);
+} 
+sum(20,14);
