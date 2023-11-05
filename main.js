f5da3102451e9a6c3ff819a76a5c14b4d6013f1a
MadhushanTharanga <madhuwa07@gmail.com>
2023-11-05 21:22:08 +0530

    create UI

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-// function printData(name ="madhushan"){
+let level=1;
+let min=0;
+let sec=0;
+let timeInterval=null;
 
-//     console.log( 'heloo '+name );
-    
-// }
+const selectElement = document.getElementById('level-select');
+const secElement = document.getElementById('sec');
+const minElement = document.getElementById('min');
 
-// printData('hutooo');
-// printData('pakaaa');  
-// printData();  
+selectElement.addEventListener("change",function(){
+    console.log(selectElement.value);
+});
 
-const sum = function(num1,num2){
-    console.log(num1+num2);
-} 
-sum(20,14);
+const start=()=>{
+    manageTime();
+}
+const manageTime=()=>{
+    min=0;
+    sec=0;
+
+    secElement.textContent='00';
+    minElement.textContent='00';
+    clearInterval(timeInterval);
+
+    timeInterval=setInterval(()=>{
+       sec++;
+
+       if (sec<10) {
+        secElement.textContent='0'+sec;
+       }
+       else{
+        secElement.textContent=sec+'';
+       }
+       if (sec==60) {
+        sec=0;
+        min++;
+        minElement.textContent='0'+min;
+       }
+       if (min==3) {
+        min=0;
+       }
+
+    },1000);
+}
